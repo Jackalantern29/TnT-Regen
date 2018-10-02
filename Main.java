2ef0b4ed07f3b9216c4eb96d588e20801929fc95
Jackalantern29 <mikmck29@gmail.com>
2018-10-02 04:17:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -15,6 +16,7 @@ import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,9 +25,8 @@ import org.bukkit.scheduler.BukkitTask;
 
 public class Main extends JavaPlugin implements Listener {
 	static List<HashMap<Location, HashMap<Material, BlockData>>> storedBlocks = new ArrayList<>();
-	static int storedBlocksCount = 0;
 	public void onLoad() {
-		File configFile = new File(getDataFolder() + "/config.yml"); 	
+		File configFile = new File(getDataFolder() + "/config.yml"); 
 		YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
 		if(!configFile.exists()) {
 			saveDefaultConfig();			
@@ -38,16 +39,22 @@ public class Main extends JavaPlugin implements Listener {
 			if(!config.contains("delayCreeper")) config.set("delayCreeper", 1200);
 			if(!config.contains("periodCreeper")) config.set("periodCreeper", 20);
 			List<String> worlds = new ArrayList<>();
-			worlds.add(getServer().getWorlds().get(0).getName());
+			if(!getServer().getWorlds().isEmpty())
+				worlds.add(getServer().getWorlds().get(0).getName());
+			else
+				worlds.add("world");
 			if(!config.contains("worlds")) config.set("worlds", worlds);
+			if(!config.contains("instantRegen")) config.set("instantRegen", false);
+			if(!config.contains("disableExplosionBlockDamage")) config.set("disableExplosionBlockDamage", false);
+			if(!config.contains("enableParticles")) config.set("enableParticles", true);
+			if(!config.contains("particle")) config.set("particle", "heart");
+			if(!config.contains("triggers.minY")) config.set("triggers.minY", 0.0);
+			if(!config.contains("triggers.maxY")) config.set("triggers.maxY", 256.0);
 			try {
 				config.save(configFile);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			if(!config.contains("instantRegen")) config.set("instantRegen", false);
-			if(!config.contains("triggers.minY")) config.set("triggers.minY", 0.0);
-			if(!config.contains("triggers.maxY")) config.set("triggers.maxY", 256.0);
 		}
 	}
 	public void onEnable() {
@@ -72,7 +79,8 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	public void instantRegen(List<Block> blockList, long delay) {
 		HashMap<Location, HashMap<Material, BlockData>> blocks = new HashMap<>();
-
+		File configFile = new File(getDataFolder() + "/config.yml"); 
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
 		for(Block b : blockList) {
 			HashMap<Material, BlockData> map = new HashMap<>();
 			map.put(b.getType(), b.getBlockData());
@@ -86,6 +94,8 @@ public class Main extends JavaPlugin implements Listener {
 						for(Material mat : blocks.get(locs).keySet()) {
 							locs.getBlock().setType(mat);
 							locs.getBlock().setBlockData(blocks.get(locs).get(mat));
+							if(config.getBoolean("enableParticles"));
+								locs.getWorld().spawnParticle(Particle.valueOf(config.getString("particle").toUpperCase()), locs, 1, 1, 1, 1);
 						}
 					}
 				}
@@ -93,17 +103,16 @@ public class Main extends JavaPlugin implements Listener {
 		}
 	}
 	public BukkitTask regenSched(List<Block> blockList, long delay, long period) {
+		File configFile = new File(getDataFolder() + "/config.yml"); 
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
 		HashMap<Location, HashMap<Material, BlockData>> blocks = new HashMap<>();
-		
 		for(Block b : blockList) {
 			HashMap<Material, BlockData> map = new HashMap<>();
 			map.put(b.getType(), b.getBlockData());
 			blocks.put(b.getLocation(), map);
 		}
-		storedBlocks.add(storedBlocksCount, blocks);
-		storedBlocksCount++;
+		storedBlocks.add(blocks);
 		return new BukkitRunnable() {
-			int count = storedBlocksCount - 1;
 			@Override
 			public void run() {
 				if(blocks.keySet().iterator().hasNext()) {
@@ -113,21 +122,27 @@ public class Main extends JavaPlugin implements Listener {
 					if((location.getBlock().getType() == Material.AIR) || (location.getBlock().getType() == Material.WATER) || (location.getBlock().getType() == Material.LAVA)) {
 						location.getBlock().setType(material, false);
 						location.getBlock().setBlockData(blockData, false);
+						if(config.getBoolean("enableParticles"))
+							location.getWorld().spawnParticle(Particle.valueOf(config.getString("particle").toUpperCase()), location, 3, 1, 1, 1);
 					}
 					
 					blocks.remove(blocks.keySet().iterator().next());
 				} else {
-					storedBlocks.remove(count);
+					storedBlocks.remove(storedBlocks.size() - 1);
 					cancel();
 				}
 			}
 		}.runTaskTimer(this, delay, period);
 	}
-	@EventHandler
+	@EventHandler(priority=EventPriority.HIGH)
 	public void onTnTExplode(EntityExplodeEvent event) {
 		Entity e = event.getEntity();
 		File configFile = new File(getDataFolder() + "/config.yml"); 
 		YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+		if(config.getBoolean("disableExplosionBlockDamage")) {
+			event.setCancelled(true);
+			return;
+		}
 		for(String worlds : config.getStringList("worlds")) {
 			if(e.getWorld().getName().equals(worlds)) {
 				if(e.getLocation().getY() >= config.getDouble("triggers.minY") && e.getLocation().getY() <= config.getDouble("triggers.maxY")) {
